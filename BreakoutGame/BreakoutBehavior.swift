12e567e708bbc97aca7f11a4f0a3f3f0f1cf6360
Murty Gudipati <murty.gudipati@saven.in>
2015-03-27 12:58:58 +0530

    added functionality to reset ball if it goes out of bounds

--- BreakoutGame/BreakoutBehavior.swift
+++ BreakoutGame/BreakoutBehavior.swift
@@ -8,17 +8,100 @@
 
 import UIKit
 
-class BreakoutBehavior: UIDynamicBehavior
+class BreakoutBehavior: UIDynamicBehavior, UICollisionBehaviorDelegate
 {
-    var gravity = UIGravityBehavior()
+    lazy var gravity: UIGravityBehavior = {
+        let lazyGravity = UIGravityBehavior()
+        lazyGravity.gravityDirection = CGVector(dx: 0.5, dy: -0.5)
+        return lazyGravity
+    }()
     
+    lazy var collider: UICollisionBehavior = {
+        let lazyCollider = UICollisionBehavior()
+        lazyCollider.collisionDelegate = self
+        lazyCollider.action = { [unowned self] in
+            if let ball = lazyCollider.items.first as? UIView {
+                if let rect1 = lazyCollider.dynamicAnimator?.referenceView?.frame {
+                    let rect2 = ball.frame
+                    if !CGRectContainsRect(rect1, rect2) {
+                        self.removeBall(ball)
+                    }
+                }
+            }
+        }
+        return lazyCollider
+    }()
+
+    lazy var pusher: UIPushBehavior = {
+        let lazyPusher = UIPushBehavior(items: [], mode: UIPushBehaviorMode.Instantaneous)
+        lazyPusher.angle = CGFloat(M_PI / 4)
+        lazyPusher.magnitude = CGFloat(-0.1)
+        return lazyPusher
+        }()
+
+    lazy var ballBehavior: UIDynamicItemBehavior = {
+        let lazyBehavior = UIDynamicItemBehavior()
+        lazyBehavior.elasticity = 1.0
+        lazyBehavior.allowsRotation = false
+        lazyBehavior.friction = 0.0
+        lazyBehavior.resistance = 0.0
+        return lazyBehavior
+    }()
+
     override init() {
         super.init()
-        addChildBehavior(gravity)
+        addChildBehavior(collider)
+        addChildBehavior(pusher)
+        addChildBehavior(ballBehavior)
     }
 
     func addBall(ball: UIView) {
         dynamicAnimator?.referenceView?.addSubview(ball)
-        gravity.addItem(ball)
+        pusher.addItem(ball)
+        collider.addItem(ball)
+        ballBehavior.addItem(ball)
+    }
+    
+    func removeBall(ball: UIView) {
+        collider.removeItem(ball)
+        pusher.removeItem(ball)
+        ballBehavior.removeItem(ball)
+        ball.removeFromSuperview()
+    }
+    
+    var bricks = [String:UIView]()
+    
+    func setBricks(newBricks: [[UIView]]) {
+        for brick in bricks {
+            collider.removeBoundaryWithIdentifier(brick.0)
+        }
+        
+        for row in 0 ..< newBricks.count {
+            for col in 0 ..< newBricks[row].count {
+                var name = "Brick \(row),\(col)"
+                var view = newBricks[row][col]
+                bricks[name] = view
+                if view.hidden != true {
+                    var path = UIBezierPath(rect: view.frame)
+                    addBarrier(path, named: name)
+                }
+            }
+        }
+    }
+    
+    func addBarrier(path: UIBezierPath, named name: String) {
+        collider.removeBoundaryWithIdentifier(name)
+        collider.addBoundaryWithIdentifier(name, forPath: path)
+    }
+    
+    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item: UIDynamicItem, withBoundaryIdentifier identifier: NSCopying, atPoint p: CGPoint) {
+        if let boundary = identifier as? String {
+            if boundary.rangeOfString("Brick") != nil {
+                collider.removeBoundaryWithIdentifier(boundary)
+                if let view = bricks[boundary] {
+                    view.hidden = true
+                }
+            }
+        }
     }
 }
--- BreakoutGame/BreakoutViewController.swift
+++ BreakoutGame/BreakoutViewController.swift
@@ -25,12 +25,10 @@ class BreakoutViewController: UIViewController
         return CGSize(width: w, height: h)
     }
 
-    var ball: UIView?
     var ballSize: CGSize {
         return CGSize(width: 20, height: 20)
     }
     
-    var paddle: UIView?
     var paddleSize: CGSize {
         let w = gameView.bounds.size.width / 4
         let h = brickHeight / 2
@@ -47,21 +45,16 @@ class BreakoutViewController: UIViewController
         for row in 0 ..< numRows {
             bricks.append([])
             for col in 0 ..< bricksPerRow {
-                bricks[row].append(UIView())
-                gameView.addSubview(bricks[row][col])
+                var view = UIView()
+                view.backgroundColor = UIColor.blueColor()
+                bricks[row].append(view)
+                gameView.addSubview(view)
             }
         }
-        
-        // add ball...
-        ball = UIView()
-        gameView.addSubview(ball!)
-        
-        // add paddle...
-        paddle = UIView()
-        gameView.addSubview(paddle!)
     }
     
     override func viewDidLayoutSubviews() {
+        println("viewDidLayoutSubviews")
         super.viewDidLayoutSubviews()
         
         // layout bricks...
@@ -72,22 +65,115 @@ class BreakoutViewController: UIViewController
             for col in 0 ..< bricksPerRow {
                 brickFrame.origin.x = pad + (brickSize.width + pad) * CGFloat(col)
                 bricks[row][col].frame = brickFrame
-                bricks[row][col].backgroundColor = UIColor.blueColor()
             }
         }
+        breakoutBehavior.setBricks(bricks)
         
-        // layout ball and paddle...
-        var ballFrame = CGRect(x: gameView.frame.midX - ballSize.width/2,
-            y: gameView.frame.maxY - ballSize.height - paddleSize.height - pad,
-            width: ballSize.width, height: ballSize.height)
-        ball!.backgroundColor = UIColor.redColor()
-        ball?.frame = ballFrame
+        if paddle == nil {
+            paddle = addPaddle()
+            paddle!.backgroundColor = UIColor.blackColor()
+            var path = UIBezierPath(rect: paddle!.frame)
+            breakoutBehavior.addBarrier(path, named: BoundaryNames.Paddle)
+        }
+        
+        if ball == nil {
+            ball = addBall()
+            ball!.backgroundColor = UIColor.redColor()
+        }
 
-        var paddleFrame = CGRect(x: gameView.frame.midX - paddleSize.width/2,
-            y: gameView.frame.maxY - paddleSize.height - pad,
-            width: paddleSize.width, height: paddleSize.height)
-        paddle!.backgroundColor = UIColor.blackColor()
-        paddle?.frame = paddleFrame
+        // add the wall barriers...
+        var path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: gameView.frame.minX, y: gameView.frame.maxY))
+        path.addLineToPoint(CGPoint(x: gameView.frame.minX, y: gameView.frame.minY))
+        breakoutBehavior.addBarrier(path, named: BoundaryNames.LeftWall)
+        
+        path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: gameView.frame.minX, y: gameView.frame.minY))
+        path.addLineToPoint(CGPoint(x: gameView.frame.maxX, y: gameView.frame.minY))
+        breakoutBehavior.addBarrier(path, named: BoundaryNames.TopWall)
+
+        path = UIBezierPath()
+        path.moveToPoint(CGPoint(x: gameView.frame.maxX, y: gameView.frame.minY))
+        path.addLineToPoint(CGPoint(x: gameView.frame.maxX, y: gameView.frame.maxY))
+        breakoutBehavior.addBarrier(path, named: BoundaryNames.RightWall)
+    }
+    
+    struct Constants {
+        static let BallSize = CGSize(width: 20, height: 20)
+        static let PaddleSize = CGSize(width: 60, height: 15)
+        static let Padding = CGFloat(40)
+    }
+    
+    var paddle: UIView?
+
+    func addPaddle() -> UIView {
+        let paddle = UIView(frame: CGRect(origin: CGPointZero, size: Constants.PaddleSize))
+        paddle.center = CGPoint(x: gameView.bounds.midX, y: gameView.bounds.maxY - Constants.Padding)
+        gameView.addSubview(paddle)
+        return paddle
+    }
+  
+    var ball: UIView?
+  
+    func addBall() -> UIView {
+        let ball = UIView(frame: CGRect(origin: CGPointZero, size: Constants.BallSize))
+        if paddle != nil {
+            ball.center = CGPoint(x: paddle!.center.x, y: paddle!.frame.minY - paddle!.frame.size.height)
+        }
+        gameView.addSubview(ball)
+
+        return ball
+    }
+    
+    struct BoundaryNames {
+        static let Paddle = "Paddle Boundary"
+        static let LeftWall = "Left Wall Boundary"
+        static let RightWall = "Right Wall Boundary"
+        static let TopWall = "Top Wall Boundary"
+        static let BottomWall = "Bottom Wall Boundary"
+        static let AllWalls = "All Wall Boundary"
+        static let LeftTopRightWall = "Left Top Right Wall Boundary"
+    }
+
+    @IBAction func breakout(sender: UITapGestureRecognizer) {
+        breakoutBehavior.addBall(ball!)
+    }
+    
+    var attachment: UIAttachmentBehavior? {
+        willSet {
+            animator.removeBehavior(attachment)
+        }
+        didSet {
+            if attachment != nil {
+                animator.addBehavior(attachment)
+                attachment?.action = { [unowned self] in
+                    if self.paddle?.frame.maxX > self.gameView.frame.maxX {
+                        self.paddle?.frame = CGRect(x: self.gameView.frame.maxX - self.paddleSize.width - self.pad,
+                            y: self.gameView.frame.maxY - self.paddleSize.height - self.pad, width: self.paddleSize.width, height: self.paddleSize.height)
+                    } else if self.paddle?.frame.minX < self.gameView.frame.minX {
+                        self.paddle?.frame = CGRect(x: self.gameView.frame.minX + self.pad,
+                            y: self.gameView.frame.maxY - self.paddleSize.height - self.pad, width: self.paddleSize.width, height: self.paddleSize.height)
+                    }
+                    let path = UIBezierPath(rect: self.paddle!.frame)
+                    self.breakoutBehavior.addBarrier(path, named: BoundaryNames.Paddle)
+                }
+            }
+        }
+    }
+    
+    @IBAction func movePaddle(sender: UIPanGestureRecognizer) {
+        var gesturePoint = sender.locationInView(gameView)
+        var anchorPoint = CGPoint(x: gesturePoint.x, y: paddle!.center.y)
+
+        switch sender.state {
+        case .Began:
+            attachment = UIAttachmentBehavior(item: paddle!, attachedToAnchor: anchorPoint)
+        case .Changed:
+            attachment?.anchorPoint = anchorPoint
+        case .Ended:
+            attachment = nil
+        default: break
+        }
     }
     
     @IBOutlet weak var gameView: BezierPathsView!
